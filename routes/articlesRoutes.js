a1daa629709b81e9ce4cbc29df5d0c7f7ec37aae
Cesario87 <ces.albred@gmail.com>
2023-03-06 18:15:22 +0100

    added/browser

--- routes/articlesRoutes.js
+++ routes/articlesRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const articlesRouter = express.Router();
-console.log('articlesRouter:', articlesRouter);
 const articlesController = require('../controllers/articlesController');
 
 articlesRouter.get('/articles', articlesController.renderArticles);
