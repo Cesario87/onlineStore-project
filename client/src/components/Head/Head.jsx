07d36ac6fcc0ff9dfa83e28ae63f0070da5b1dad
Cesario87 <ces.albred@gmail.com>
2023-03-07 21:38:21 +0100

    added/more styles

--- client/src/components/Head/Head.jsx
+++ client/src/components/Head/Head.jsx
@@ -6,8 +6,8 @@ const Header = () => {
   const numberCart = useSelector(state => state.numberItems);
   return (
     <header className="formatHead">
-      <h3>fakeStore©</h3>
-      <Link to="/"><h1>Products</h1></Link>
+      <Link to="/"><h3>fakeStore©</h3></Link>
+      {/* <Link to="/"><h1>Products</h1></Link> */}
       <Link to="/cart" title='Shopping cart' id='cartIconContainer'>
         <span id="numberCarrito">{numberCart}</span>
         <img id="carrito" src="https://cdn-icons-png.flaticon.com/512/891/891462.png" alt="shopping cart" />
--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -1,36 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Routes, Route } from "react-router-dom";
 import List from './List';
 import Cart from './Cart';
 
 const Main = ({ globalSortCriteria, globalSortOrder, handleGlobalSortCriteriaChange, handleGlobalSortOrderChange }) => {
+  const [activeSortCriteria, setActiveSortCriteria] = useState(null);
+  const [activeSortOrder, setActiveSortOrder] = useState(null);
+
   const updateSortCriteria = (criteria) => {
     handleGlobalSortCriteriaChange(criteria);
+    setActiveSortCriteria(criteria);
   };
 
   const updateSortOrder = (order) => {
     handleGlobalSortOrderChange(order);
+    setActiveSortOrder(order);
+  };
+
+  const isSortCriteriaActive = (criteria) => {
+    return activeSortCriteria === criteria;
+  };
+
+  const isSortOrderActive = (order) => {
+    return activeSortOrder === order;
   };
 
   return (
     <main className="mainFormat">
       <div id="botonera">
         <p>Sort by:</p>
-          <button onClick={() => updateSortCriteria('name')}>
-            Name
-          </button>
-          <button onClick={() => updateSortCriteria('price')}>
-            Price
-          </button>
-          <button onClick={() => updateSortCriteria('valoration')}>
-            Valoration
-          </button>
-          <button onClick={() => updateSortOrder('asc')}>
-            ⬆️
-          </button>
-          <button onClick={() => updateSortOrder('desc')}>
-            ⬇️
-          </button>
+        <button className={isSortCriteriaActive('name') ? 'active' : ''} onClick={() => updateSortCriteria('name')}>
+          Name
+        </button>
+        <button className={isSortCriteriaActive('price') ? 'active' : ''} onClick={() => updateSortCriteria('price')}>
+          Price
+        </button>
+        <button className={isSortCriteriaActive('valoration') ? 'active' : ''} onClick={() => updateSortCriteria('valoration')}>
+          Stars ★
+        </button>
+        <span>|</span>
+        <button id="buttonUp" className={isSortOrderActive('asc') ? 'active' : ''} onClick={() => updateSortOrder('asc')}>
+          ⬆
+        </button>
+        <button id="buttonDown" className={isSortOrderActive('desc') ? 'active' : ''} onClick={() => updateSortOrder('desc')}>
+          ⬇
+        </button>
       </div>
       <Routes>
         <Route path="/" element={<List sortCriteria={globalSortCriteria} sortOrder={globalSortOrder} />} />
