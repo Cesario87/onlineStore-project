37fe8a6d2fa980a74a22430a1c76dc52a0eb5c70
Cesario87 <ces.albred@gmail.com>
2023-04-05 18:12:02 +0200

    fix

--- config/sqlConnection.js
+++ config/sqlConnection.js
@@ -1,37 +1,76 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-if (!process.env.DB_USER || !process.env.DB_PASSWORD || !process.env.DB_HOST) {
-    console.error('Error: Por favor, asegúrese de que todas las variables de entorno necesarias estén definidas.');
-    process.exit(1);
+if (!process.env.DB_URL) {
+  console.error('Error: Por favor, asegúrese de que la variable de entorno DB_URL esté definida.');
+  process.exit(1);
 }
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    logging: false,
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
+const db = new Sequelize(process.env.DB_URL, {
+  dialect: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
+  }
 });
 
 console.log(db);
 
 const connectSQL = async () => {
-    try {
-        await db.authenticate();
-        console.log('PostgreSQL database connected...');
-    } catch (error) {
-        console.error('Unable to connect to SQL database:', error);
-    }
+  try {
+    await db.authenticate();
+    console.log('PostgreSQL database connected...');
+  } catch (error) {
+    console.error('Unable to connect to SQL database:', error);
+  }
 };
 
 connectSQL();
 
 module.exports = {
-    connectSQL,
-    db
+  connectSQL,
+  db
 }
+
+
+
+// const { Sequelize } = require('sequelize');
+// require('dotenv').config();
+
+// if (!process.env.DB_USER || !process.env.DB_PASSWORD || !process.env.DB_HOST) {
+//     console.error('Error: Por favor, asegúrese de que todas las variables de entorno necesarias estén definidas.');
+//     process.exit(1);
+// }
+
+// const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//     host: process.env.DB_HOST,
+//     dialect: 'postgres',
+//     logging: false,
+//     dialectOptions: {
+//         ssl: {
+//             require: true,
+//             rejectUnauthorized: false
+//         }
+//     }
+// });
+
+// console.log(db);
+
+// const connectSQL = async () => {
+//     try {
+//         await db.authenticate();
+//         console.log('PostgreSQL database connected...');
+//     } catch (error) {
+//         console.error('Unable to connect to SQL database:', error);
+//     }
+// };
+
+// connectSQL();
+
+// module.exports = {
+//     connectSQL,
+//     db
+// }
